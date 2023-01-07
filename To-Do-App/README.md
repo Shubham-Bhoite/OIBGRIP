068e569bf08e2ec1f7f16a5291401ef7981275bf
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-01-07 17:26:25 +0530

    upload

---
+++ To-Do-App/README.md
@@ -0,0 +1,5 @@
+# To-Do App
+
+- ToDo List App is a used to maintain our day-to-day tasks with the most important tasks at the top of the list, and the least important tasks at the bottom. 
+
+- It is helpful in planning our daily schedules.
---
+++ To-Do-App/script.js
@@ -0,0 +1,44 @@
+document.querySelector("#input").addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+      const input = document.querySelector("#input");
+      addItem(input.value);
+    }
+      
+  });
+  
+  document.querySelector("#add_item").addEventListener("click", () => {
+    const input = document.querySelector("#input");
+    addItem(input.value);
+  });
+  
+  addItem = (input) => {
+    const item = document.createElement("div");
+    const div = document.createElement("div");
+    const checkIcon = document.createElement("i");
+    const trashIcon = document.createElement("i");
+    const text = document.createElement("p");
+  
+    item.className = "item";
+    text.textContent = input;
+  
+    checkIcon.className = "fas fa-check-square";
+    checkIcon.style.color = "lightgray";
+    checkIcon.addEventListener("click", () => {
+      checkIcon.style.color = "limegreen";
+    })
+    div.appendChild(checkIcon);
+  
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", () => {
+      item.remove();
+    })
+    div.appendChild(trashIcon);
+  
+    item.appendChild(text);
+    item.appendChild(div);
+  
+    document.querySelector("#to_do_list").appendChild(item);
+    document.querySelector("#input").value = "";
+  }
+  
